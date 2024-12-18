3cfdaa0864f9a5f5ca52e7b96d65f29ecff3062e
su-rays <sureshsharki123@gmail.com>
2024-12-18 07:36:29 +0530

    changed indexing

--- DSA/IndexingMethods/treeBasedIndexing.py
+++ DSA/IndexingMethods/treeBasedIndexing.py
@@ -1,13 +1,3 @@
-class LinearIndexing:
-    def __init__(self, data):
-        self.data = data
-
-    def search(self, key):
-        for index, value in enumerate(self.data):
-            if value == key:
-                return index
-        return -1
-
 class TreeNode:
     def __init__(self, key):
         self.key = key
@@ -35,39 +25,29 @@ class BinarySearchTree:
                 node.right = TreeNode(key)
             else:
                 self._insert(node.right, key)
-
+    
     def search(self, key):
         return self._search(self.root, key)
-
+    
     def _search(self, node, key):
         if node is None:
             return None
-        if node.key == key:
+        elif node.key == key:
             return node
         elif key < node.key:
             return self._search(node.left, key)
         else:
             return self._search(node.right, key)
         
-if __name__ == '__main__':
-    ''' Linear Indexing '''
-    key = 30
+if __name__ == "__main__":
     data = [10, 20, 30, 40, 50]
-    indexer = LinearIndexing(data)
-
-    ''' Tree Based Indexing '''
+    key = 60
     bst = BinarySearchTree()
-    bst.insert(10)
-    bst.insert(20)
-    bst.insert(30)
-    bst.insert(40)
-    bst.insert(50)
-
-    index = indexer.search(key)
+    for val in data:
+        bst.insert(val)
+    print("all values are inserted")
     found_node = bst.search(key)
-
-    if index != -1 or found_node:
-        print(f"Key {key} found")
+    if found_node:
+        print(f"key {key} found in data {data} ")
     else:
-        print(f"Key {key} not found.")
-
+        print(f"key {key} not found in {data}")
